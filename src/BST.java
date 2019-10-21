65b79448eab62c917d3a3eca5980ef1f337406c8
Alex-wojcik <alex.wojcik@coverhound.com>
2019-10-21 13:47:02 -0700

    Update BST.java

--- src/BST.java
+++ src/BST.java
@@ -1,26 +1,33 @@
-import javax.swing.*;
-import java.net.HttpRetryException;
+package src;
 
 public class BST<T> {
-	
-	public class BSTNode {
+	/**
+	 * Definition for a binary tree node.
+	 * public class TreeNode {
+	 *     Comparable data;
+	 *     TreeNode left;
+	 *     TreeNode right;
+	 *     TreeNode(Comparable data) { data = data; }
+	 * }
+	 */
+	public class TreeNode {
 		Comparable data;
-		BSTNode left;
-		BSTNode right;
+		TreeNode left;
+		TreeNode right;
 		
-		public BSTNode (Comparable data){
-			this.data=data;
+		public TreeNode(Comparable element){
+			this.data=element;
 			this.right = this.left = null;
 		}
 	}
 	
-	public BSTNode root;
+	public TreeNode root;
 	
 
 	
 	public void insert (Comparable element) {
-		
-		insert (root, element);
+
+		root = insert (root, element);
 	}
 	
 	public boolean find (Comparable element) {
@@ -28,7 +35,9 @@ public class BST<T> {
 	}
 	
 	
-	public void delete (T insertable) {
+	public void delete (Comparable element) {
+		root = delete(root, element);
+
 	}
 	
 	public void print () {
@@ -36,10 +45,9 @@ public class BST<T> {
 	}
 	
 	
-	private BSTNode insert(BSTNode tree, Comparable element){
+	private TreeNode insert(TreeNode tree, Comparable element){
 		if(tree==null){
-			System.out.print ("im here");
-			return new BSTNode (element);
+			return new TreeNode(element);
 		}
 		if(element.compareTo (tree.data) < 0) {
 			tree.left = insert (tree.left, element);
@@ -49,7 +57,7 @@ public class BST<T> {
 			return tree;
 		}
 	}
-	private boolean find(BSTNode tree, Comparable element){
+	private boolean find(TreeNode tree, Comparable element){
 		if(tree == null){
 			return false;
 		}
@@ -62,11 +70,49 @@ public class BST<T> {
 			return find (tree.right, element);
 		}
 	}
-	
-	private void print(BSTNode tree){
+	private TreeNode delete(TreeNode tree, Comparable element){
+		if(tree == null){
+			return null;
+		}
+		if(tree.data.compareTo(element) == 0){
+			if(tree.left == null){
+				return tree.right;
+			}else if(tree.right == null){
+				return tree.left;
+			}
+			else {
+				if(tree.right.left == null){
+					tree.data = tree.right.data;
+					tree.right = tree.right.right;
+					return tree;
+				}else{
+					tree.data = removeSmallest(tree.right);
+					return tree;
+				}
+			}
+		}else if(element.compareTo(tree.data) < 0){
+			tree.left = delete(tree.left, element);
+			return tree;
+		}else{
+			tree.right = delete(tree.right, element);
+			return tree;
+		}
+	}
+
+	private Comparable removeSmallest(TreeNode tree){
+		if(tree.left.left == null){
+			Comparable smallest = tree.left.data;
+			tree.left = tree.left.right;
+			return smallest;
+		} else {
+			return removeSmallest(tree.left);
+		}
+	}
+
+	private void print(TreeNode tree){
 		if(tree != null){
 			print (tree.left);
-			System.out.print (tree.data);
+			System.out.println (tree.data);
 			print (tree.right);
 		}
 	}
